2102edc53e47922e1038566dc16f6bb376168b86
yael.lotan <yaelotan19@gmail.com>
2020-06-05 10:45:06 +0300

    feat(Routes.js): add router setup

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { Provider } from 'react-redux'
 import theme from './theme/index';
+import { browserHistory } from './store';
+import { Router, BrowserRouter } from 'react-router-dom';
 
 import { store } from './store';
-
-import Home from './screens/Home';
+import Routes from './Routes';
 
 function App() {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <Home />
+        <BrowserRouter>
+          <Router history={browserHistory}>
+            <Routes />
+          </Router>
+        </BrowserRouter>
       </ThemeProvider>
     </Provider>
   );
